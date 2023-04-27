5cd47e03ab6e66dc2213ff2ab117e14ef45a31a7
Aldo Cedillo <a.cedillo@hlcn.mx>
2023-04-27 07:53:32 -0600

    Blink id scanning coden
    Signed-off-by: Aldo Cedillo <a.cedillo@hlcn.mx>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@microblink/blinkid-in-browser-sdk/ui/loader";
+import "./App.css";
 
 function App() {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    applyPolyfills().then(() => {
+      defineCustomElements().then(() => {
+        el.current.licenseKey = "";
+        el.current.recognizers = ["BlinkIdMultiSideRecognizer"];
+        el.current.recognizerOptions = {
+          BlinkIdMultiSideRecognizer: {
+            returnFullDocumentImage: true,
+            returnFullDocumentFrontImage: true,
+            returnFullDocumentBackImage: true,
+            returnFaceImage: true,
+            returnSignatureImage: true,
+          },
+        };
+        // el.current.recognizerOptions = {
+        //   BlinkIdSingleSideRecognizer: {
+        //     returnFullDocumentImage: true,
+        //     returnFaceImage: true,
+        //   },
+        // };
+        // el.current.recognizers = [{
+        //   recognizerType: "BlinkIdMultiSideRecognizer",
+        //   // returnFullDocumentImage: true,
+        // }];
+
+        // Engine location depends on the actual location of WebAssembly resources
+        el.current.engineLocation = window.location.origin + "/resources";
+        // el.current.engineLocation = window.location.origin + `/resources/blinkid.worker.min.js`;
+        // el.current.settings = {
+        //   returnFullDocumentImage: true,
+        // }
+        // console.log('>>>>>>>>>>>>current: ', el.current.recognizers);
+        // console.log('>>>>>>>>>>>>current.settings: ', el.current.settings);
+        // console.log('>>>>>>>>>>>>current.translations: ', el.current.translations);
+
+        el.current.addEventListener("ready", (ev) => {
+          console.log("ready.1: ", ev.details);
+          console.log("ready.2: ", ev);
+        });
+
+        el.current.addEventListener("scanSuccess", (ev) => {
+          console.log("scanSuccess.1: ", ev.details);
+          console.log("scanSuccess.2: ", ev);
+          console.log("scanSuccess.3: ", JSON.stringify(ev));
+        });
+
+        el.current.addEventListener("scanError", (ev) => {
+          console.log("scanError", ev.details);
+        });
+
+        el.current.addEventListener("fatalError", (ev) => {
+          console.log("fatalError", ev.details);
+        });
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +81,7 @@ function App() {
         >
           Learn React
         </a>
+        <blinkid-in-browser ref={el}></blinkid-in-browser>
       </header>
     </div>
   );
